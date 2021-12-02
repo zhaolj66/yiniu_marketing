1a69692105de7c2f1affd061a66e410086c01947
zlj <1072611435@qq.com>
2021-12-02 15:55:11 +0800

    产生自定义用户画像

--- dynamic_rule_engine/src/main/java/cn/zlj/dynamic_rule/datagen/UserProfileGen.java
+++ dynamic_rule_engine/src/main/java/cn/zlj/dynamic_rule/datagen/UserProfileGen.java
@@ -1,8 +1,45 @@
 package cn.zlj.dynamic_rule.datagen;
 
-public class UserProfileGen {
-    public static void main(String[] args) {
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class UserProfileGen {
+    public static void main(String[] args) throws Exception {
+        Configuration configuration = HBaseConfiguration.create();
+        configuration.set("hbase.zookeeper.quorum","hadoop102:2181,hadoop103:2181,hadoop104:2181");
+        Connection conn = ConnectionFactory.createConnection(configuration);
+        Table user_perfile = conn.getTable(TableName.valueOf("user_profile"));
+        int i = 0;
+        ArrayList<Put> puts = new ArrayList<>();
+        while(true){
+            i++;
+            String account = StringUtils.leftPad(i+"",6,"0") ;
+            Put put = new Put(Bytes.toBytes(account));//确定rowkey
+            for (int j = 0; j <500 ; j++) {
+                put.addColumn(Bytes.toBytes("f"),Bytes.toBytes("k"+RandomUtils.nextInt(1,500)),Bytes.toBytes("v"+RandomUtils.nextInt(1,100)));
+            }
+
+            puts.add(put);
+            if (i%20==0){
+                user_perfile.put(puts);
+                puts.clear();
+                System.out.println(i);
+            }
+
+        }
+//        if (!conn.isClosed()) {
+//            conn.close();
+//        }
 
     }
 }
